2e052e52b60c63371d289986fa3182ea20a8d84b
raydenm <raydenm928@gmail.com>
2024-03-27 13:44:39 +0800

    feat: change Button

--- app/page.tsx
+++ app/page.tsx
@@ -33,11 +33,11 @@ export default function Web() {
               Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
               enjoyable development process.
             </p>
-            <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
+            <Button href="https://github.com/raydenm/next-enterprise-template" className="mr-3">
               Get started
             </Button>
             <Button
-              href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
+              href="https://vercel.com/new/git/external?repository-url=https://github.com/raydenm/next-enterprise-template"
               intent="secondary"
             >
               Deploy Now
